9345d182093f91d1d1499ea9f0dc224ade2e8a4f
Mahavishnu721 <mahavishnu721@gmail.com>
2026-01-12 15:14:28 +0530

    init

---
+++ Datatype/worksheet1/worksheet1.py
@@ -0,0 +1,14 @@
+def Q1():
+    a=20
+    b=10
+    print("add :", a+ b)
+    print("sub :", a- b)
+    print("mul :", a* b)
+    print("div :", a/ b)
+    print("quotient :", a// b)
+    print("product :", a** b)
+def Q2():
+    
+
+if __name__=="__main__":
+    Q1()
